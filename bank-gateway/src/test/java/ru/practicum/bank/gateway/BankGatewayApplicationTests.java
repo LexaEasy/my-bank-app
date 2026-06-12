70cca0d5e5722d3c2b7f875bd7d61d5d9caa95f4
Your Name <you@example.com>
2026-06-12 20:26:44 +0300

    Добавить зависимости Gateway

---
+++ bank-gateway/src/test/java/ru/practicum/bank/gateway/BankGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.practicum.bank.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
