2cc37eae1ac4ccbbfc3fcb94fe5ed78e69eeed60
Your Name <you@example.com>
2026-06-12 19:51:59 +0300

    Добавить общие настройки Spring Boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+
 plugins {
     java
+    alias(libs.plugins.spring.boot) apply false
+    alias(libs.plugins.spring.dependency.management) apply false
 }
 
 group = "ru.practicum.bank"
 version = "0.0.1-SNAPSHOT"
 
+val springCloudVersion = libs.versions.spring.cloud.get()
+val springBootStarterTest = libs.spring.boot.starter.test
+
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
     group = rootProject.group
     version = rootProject.version
@@ -19,5 +28,15 @@ subprojects {
 
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
+    }
+
+    extensions.configure<DependencyManagementExtension> {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+        }
+    }
+
+    dependencies {
+        "testImplementation"(springBootStarterTest)
     }
 }
