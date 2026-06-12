ee3131b95e843a78dbf238bbfd61cdb4e13c35ee
Your Name <you@example.com>
2026-06-12 20:41:00 +0300

    Добавить зависимости Accounts Service

--- accounts-service/build.gradle.kts
+++ accounts-service/build.gradle.kts
@@ -1,1 +1,8 @@
+dependencies {
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.web)
 
+    runtimeOnly(libs.postgresql)
+    testRuntimeOnly(libs.h2)
+}
---
+++ accounts-service/src/test/java/ru/practicum/bank/accounts/AccountsServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.practicum.bank.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
