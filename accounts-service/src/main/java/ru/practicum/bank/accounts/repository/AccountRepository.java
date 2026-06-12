02d49debaa9e9fb18f5ed6419bdf49c278efed70
Your Name <you@example.com>
2026-06-12 20:47:55 +0300

    Добавить repository аккаунтов

---
+++ accounts-service/src/main/java/ru/practicum/bank/accounts/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package ru.practicum.bank.accounts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.bank.accounts.model.Account;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByLogin(String login);
+}
