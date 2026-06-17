5bae5f19a9ead306cab5477c0e09814db28e2a3b
Your Name <you@example.com>
2026-06-17 10:21:22 +0300

    Настройки сервисов перенесены в Config Server

--- accounts-service/build.gradle.kts
+++ accounts-service/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(libs.spring.boot.starter.oauth2.resource.server)
     implementation(libs.spring.boot.starter.validation)
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.cloud.starter.config)
     implementation(libs.spring.cloud.starter.netflix.eureka.client)
 
     runtimeOnly(libs.flyway.database.postgresql)
