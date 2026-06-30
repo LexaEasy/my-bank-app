b41f8f6c00759ce913a7697299a4cc0a372ab32b
Your Name <you@example.com>
2026-06-30 13:27:41 +0300

    настроены производители событий kafka

--- transfer-service/src/main/java/ru/practicum/bank/transfer/TransferServiceApplication.java
+++ transfer-service/src/main/java/ru/practicum/bank/transfer/TransferServiceApplication.java
@@ -2,7 +2,10 @@ package ru.practicum.bank.transfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import ru.practicum.bank.common.notification.NotificationProducerConfiguration;
 
+@Import(NotificationProducerConfiguration.class)
 @SpringBootApplication
 public class TransferServiceApplication {
 
