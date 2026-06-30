7fd9dd1dcb7943476ec02ba08a3a0867e8edb764
Your Name <you@example.com>
2026-06-30 12:17:05 +0300

    добавлены параметры конфигурации kafka

--- notifications-service/src/test/java/ru/practicum/bank/notifications/NotificationsServiceApplicationTests.java
+++ notifications-service/src/test/java/ru/practicum/bank/notifications/NotificationsServiceApplicationTests.java
@@ -1,17 +1,32 @@
 package ru.practicum.bank.notifications;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class NotificationsServiceApplicationTests {
+
+    @Autowired
+    private Environment environment;
 
     @MockitoBean
     private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
+        assertThat(environment.getProperty("spring.kafka.bootstrap-servers"))
+                .isEqualTo("localhost:9092");
+        assertThat(environment.getProperty("spring.kafka.consumer.group-id"))
+                .isEqualTo("bank-notifications");
+        assertThat(environment.getProperty("bank.kafka.notifications-topic"))
+                .isEqualTo("bank.notifications");
+        assertThat(environment.getProperty("bank.kafka.notifications-dlt-topic"))
+                .isEqualTo("bank.notifications.dlt");
     }
 }
--- transfer-service/src/test/java/ru/practicum/bank/transfer/TransferServiceApplicationTests.java
+++ transfer-service/src/test/java/ru/practicum/bank/transfer/TransferServiceApplicationTests.java
@@ -1,17 +1,28 @@
 package ru.practicum.bank.transfer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class TransferServiceApplicationTests {
+
+    @Autowired
+    private Environment environment;
 
     @MockitoBean
     private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
+        assertThat(environment.getProperty("spring.kafka.bootstrap-servers"))
+                .isEqualTo("localhost:9092");
+        assertThat(environment.getProperty("bank.kafka.notifications-topic"))
+                .isEqualTo("bank.notifications");
     }
 }
