436d32ae212ee897c0cafe8ef449273092d9f4b4
Your Name <you@example.com>
2026-07-05 00:02:28 +0300

    добавить единый json-формат логов

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.gradle.language.jvm.tasks.ProcessResources
 
 plugins {
     java
@@ -30,6 +31,13 @@ subprojects {
 
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
+        systemProperty("spring.profiles.active", "test")
+    }
+
+    if (name != "shared") {
+        tasks.named<ProcessResources>("processResources") {
+            from(rootProject.file("config/logback-spring.xml"))
+        }
     }
 
     tasks.named<BootJar>("bootJar") {
